57d9cb7761d2a993b4c2f4abab697c9a7e46ad59
rebat-yuta <zm21202.k.yuta@gmail.com>
2023-12-14 15:14:08 +0900

    まだ

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,14 +9,14 @@ class Post extends Model
 {
   use HasFactory;
   
+  protected $fillable = [
+    'title',
+    'body',
+];
   public function getByLimit(int $limit_count = 10)
 {
-    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+   return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
 }
-public function getPaginateByLimit(int $limit_count = 10)
-{
-    // updated_atで降順に並べたあと、limitで件数制限をかける
-    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-}
+
 }
 
